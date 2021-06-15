1ffa96b600faf12ceb027c6be59e7043fd7ff6d0
Enes <73299223+enescelep@users.noreply.github.com>
2021-06-15 19:03:35 +0300

    4 ve 5'in kareleri tamamlandı.

--- src/dortvebesinkuvvetleri/Main.java
+++ src/dortvebesinkuvvetleri/Main.java
@@ -5,20 +5,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number, squareOf4 = 1, sqaureOf5 = 1;
+        int number, squareOf4 = 1, squareOf5 = 1, sonuc4=1, sonuc5= 1;
         System.out.print("Please enter a number: ");
         number = scanner.nextInt();
 
         for (int i = 1; i <= number; i++) {
-            for (int j = 1; j <= number; j++) {
-                for(int k = 1; k <= number; k++) {
-                    if (j % 4 == 0) {
-                        System.out.println(j*j);
-                    }
-                    if (k % 5 == 0) {
-                        System.out.println(k*k);
-                    }
+            if (i % 4 == 0) {
+               for (int j = 1; j<= squareOf4; j++) {
+                  j*=4;
+                  j=sonuc4;
+               }
+                System.out.println(sonuc4);
+                squareOf4++;
+            }
+            if (i % 5 == 0) {
+                for (int j = 1; j<= squareOf5; j++) {
+                    j*=5;
+                    j=sonuc5;
                 }
+                squareOf5++;
+                System.out.println(sonuc5);
             }
         }
     }
