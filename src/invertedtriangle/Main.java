8c8f08a719d0387248ac5ade49d1633de555b173
Enes <73299223+enescelep@users.noreply.github.com>
2021-06-17 18:56:45 +0300

    Inverted triangle completed.

---
+++ src/invertedtriangle/Main.java
@@ -0,0 +1,27 @@
+package invertedtriangle;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please type the number of digits: ");
+        int rows = scanner.nextInt();
+
+        for(int i = rows; i >= 1; --i) {
+            for (int space = 1; space <= rows - i; ++space) {
+                System.out.print("  ");
+            }
+
+            for (int j = i; j <= 2 * i - 1; ++j) {
+                System.out.print("* ");
+            }
+
+            for (int j = 0; j < i - 1; ++j) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+}
